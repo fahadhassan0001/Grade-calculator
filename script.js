09dbe5309197cea3e47addada2782a57c8dd1434
Fahad Hassan <fahadhassan0001@gmail.com>
2024-09-14 03:21:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var score = Number(prompt("Enter the student's score"));
+
+if(score >=90){
+   alert (" The student's grade is A.")
+}
+else if(score >=80){
+alert(" The sudent's grade is B")
+}
+else if(score >=70){
+    alert(" The sudent's grade is C")
+    }
+    else if(score >=60){
+        alert(" The sudent's grade is D")
+        }
+        else{
+            alert(" The sudent's grade is F")
+            }
